8e864b8d23fc24b3d659782012b6279882f5f4da
Riley Weist <riley.weist@gmail.com>
2019-11-26 14:08:30 -0500

    Added Code

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.quitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.button1.Location = new System.Drawing.Point(66, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 129);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.button2.Location = new System.Drawing.Point(264, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 129);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.button3.Location = new System.Drawing.Point(466, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 129);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.button4.Location = new System.Drawing.Point(66, 236);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 129);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.button5.Location = new System.Drawing.Point(264, 236);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 129);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.button6.Location = new System.Drawing.Point(66, 410);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 129);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.button7.Location = new System.Drawing.Point(466, 236);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(152, 129);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.button8.Location = new System.Drawing.Point(264, 410);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(152, 129);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.button9.Location = new System.Drawing.Point(466, 410);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(152, 129);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetBtn.Location = new System.Drawing.Point(141, 620);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(160, 88);
+            this.resetBtn.TabIndex = 9;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // quitBtn
+            // 
+            this.quitBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitBtn.Location = new System.Drawing.Point(362, 620);
+            this.quitBtn.Name = "quitBtn";
+            this.quitBtn.Size = new System.Drawing.Size(160, 88);
+            this.quitBtn.TabIndex = 10;
+            this.quitBtn.Text = "Quit";
+            this.quitBtn.UseVisualStyleBackColor = true;
+            this.quitBtn.Click += new System.EventHandler(this.quitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 760);
+            this.Controls.Add(this.quitBtn);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Button quitBtn;
+    }
+}
+
---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        int[,] ttt =
+        {
+            {-1, -1, -1},
+            {-1, -1, -1},
+            {-1, -1, -1}
+        };
+        int user = 0;
+        bool buttonsUsable = true;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CheckWin()
+        {
+            if ((ttt[0,0] == ttt[0,1]) && (ttt[0,0] == ttt[0,2]) && (ttt[0,0] !=-1))
+            {
+                if (ttt[0, 0] == 0)
+                {
+                    MessageBox.Show(@"O wins!!");
+                    buttonsUsable = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"X Wins!!");
+                    buttonsUsable = false;
+                }
+            }
+            if ((ttt[0, 0] == ttt[1, 1]) && (ttt[0, 0] == ttt[2, 2]) && (ttt[0, 0] != -1))
+            {
+                if (ttt[0, 0] == 0)
+                {
+                    MessageBox.Show(@"O wins!!");
+                    buttonsUsable = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"X Wins!!");
+                    buttonsUsable = false;
+                }
+            }
+            if ((ttt[0, 0] == ttt[1, 0]) && (ttt[0, 0] == ttt[2, 0]) && (ttt[0, 0] != -1))
+            {
+                if (ttt[0, 0] == 0)
+                {
+                    MessageBox.Show(@"O wins!!");
+                    buttonsUsable = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"X Wins!!");
+                    buttonsUsable = false;
+                }
+            }
+            if ((ttt[0, 1] == ttt[1, 1]) && (ttt[0, 1] == ttt[2, 1]) && (ttt[0, 1] != -1))
+            {
+                if (ttt[0, 1] == 0)
+                {
+                    MessageBox.Show(@"O wins!!");
+                    buttonsUsable = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"X Wins!!");
+                    buttonsUsable = false;
+                }
+            }
+            if ((ttt[0, 2] == ttt[1, 2]) && (ttt[0, 2] == ttt[2, 2]) && (ttt[0, 2] != -1))
+            {
+                if (ttt[0, 2] == 0)
+                {
+                    MessageBox.Show(@"O wins!!");
+                    buttonsUsable = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"X Wins!!");
+                    buttonsUsable = false;
+                }
+            }
+            if ((ttt[0, 2] == ttt[1, 1]) && (ttt[0, 2] == ttt[2, 0]) && (ttt[0, 2] != -1))
+            {
+                if (ttt[0, 2] == 0)
+                {
+                    MessageBox.Show(@"O wins!!");
+                    buttonsUsable = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"X Wins!!");
+                    buttonsUsable = false;
+                }
+            }
+            if ((ttt[1, 0] == ttt[1, 1]) && (ttt[1, 0] == ttt[1, 2]) && (ttt[1, 0] != -1))
+            {
+                if (ttt[1, 0] == 0)
+                {
+                    MessageBox.Show(@"O wins!!");
+                    buttonsUsable = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"X Wins!!");
+                    buttonsUsable = false;
+                }
+            }
+            if ((ttt[2, 0] == ttt[2, 1]) && (ttt[2, 0] == ttt[2, 2]) && (ttt[2, 0] != -1))
+            {
+                if (ttt[2, 0] == 0)
+                {
+                    MessageBox.Show(@"O wins!!");
+                    buttonsUsable = false;
+                }
+                else
+                {
+                    MessageBox.Show(@"X Wins!!");
+                    buttonsUsable = false;
+                }
+            }
+
+            if (buttonsUsable == false)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                button7.Enabled = false;
+                button8.Enabled = false;
+                button9.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ttt[0, 0] == -1)
+            {
+                if (user == 0)
+                {
+                    ttt[0, 0] = 0;
+                    user = 1;
+                    button1.Text = (@"O");
+                }
+                else
+                {
+                    ttt[0, 0] = 1;
+                    user = 0;
+                    button1.Text = (@"X");
+                }
+                CheckWin();
+            }
+            else
+            {
+                MessageBox.Show(@"This tile is already in use.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ttt[0, 1] == -1)
+            {
+                if (user == 0)
+                {
+                    ttt[0, 1] = 0;
+                    user = 1;
+                    button2.Text = (@"O");
+                }
+                else
+                {
+                    ttt[0, 1] = 1;
+                    user = 0;
+                    button2.Text = (@"X");
+                }
+                CheckWin();
+            }
+            else
+            {
+                MessageBox.Show(@"This tile is already in use.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (ttt[0, 2] == -1)
+            {
+                if (user == 0)
+                {
+                    ttt[0, 2] = 0;
+                    user = 1;
+                    button3.Text = (@"O");
+                }
+                else
+                {
+                    ttt[0, 2] = 1;
+                    user = 0;
+                    button3.Text = (@"X");
+                }
+                CheckWin();
+            }
+            else
+            {
+                MessageBox.Show(@"This tile is already in use.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (ttt[1, 0] == -1)
+            {
+                if (user == 0)
+                {
+                    ttt[1, 0] = 0;
+                    user = 1;
+                    button4.Text = (@"O");
+                }
+                else
+                {
+                    ttt[1, 0] = 1;
+                    user = 0;
+                    button4.Text = (@"X");
+                }
+                CheckWin();
+            }
+            else
+            {
+                MessageBox.Show(@"This tile is already in use.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (ttt[1, 1] == -1)
+            {
+                if (user == 0)
+                {
+                    ttt[1, 1] = 0;
+                    user = 1;
+                    button5.Text = (@"O");
+                }
+                else
+                {
+                    ttt[1, 1] = 1;
+                    user = 0;
+                    button5.Text = (@"X");
+                }
+                CheckWin();
+            }
+            else
+            {
+                MessageBox.Show(@"This tile is already in use.");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (ttt[1, 2] == -1)
+            {
+                if (user == 0)
+                {
+                    ttt[1, 2] = 0;
+                    user = 1;
+                    button7.Text = (@"O");
+                }
+                else
+                {
+                    ttt[1, 2] = 1;
+                    user = 0;
+                    button7.Text = (@"X");
+                }
+                CheckWin();
+            }
+            else
+            {
+                MessageBox.Show(@"This tile is already in use.");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (ttt[2, 0] == -1)
+            {
+                if (user == 0)
+                {
+                    ttt[2, 0] = 0;
+                    user = 1;
+                    button6.Text = (@"O");
+                }
+                else
+                {
+                    ttt[2, 0] = 1;
+                    user = 0;
+                    button6.Text = (@"X");
+                }
+                CheckWin();
+            }
+            else
+            {
+                MessageBox.Show(@"This tile is already in use.");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (ttt[2, 1] == -1)
+            {
+                if (user == 0)
+                {
+                    ttt[2, 1] = 0;
+                    user = 1;
+                    button8.Text = (@"O");
+                }
+                else
+                {
+                    ttt[2, 1] = 1;
+                    user = 0;
+                    button8.Text = (@"X");
+                }
+                CheckWin();
+            }
+            else
+            {
+                MessageBox.Show(@"This tile is already in use.");
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (ttt[2, 2] == -1)
+            {
+                if (user == 0)
+                {
+                    ttt[2, 2] = 0;
+                    user = 1;
+                    button9.Text = (@"O");
+                }
+                else
+                {
+                    ttt[2, 2] = 1;
+                    user = 0;
+                    button9.Text = (@"X");
+                }
+                CheckWin();
+            }
+            else
+            {
+                MessageBox.Show(@"This tile is already in use.");
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            buttonsUsable = true;
+            button1.Text = (@"");
+            button2.Text = (@"");
+            button3.Text = (@"");
+            button4.Text = (@"");
+            button5.Text = (@"");
+            button6.Text = (@"");
+            button7.Text = (@"");
+            button8.Text = (@"");
+            button9.Text = (@"");
+            ttt[0, 0] = -1;
+            ttt[0, 1] = -1;
+            ttt[0, 2] = -1;
+            ttt[1, 0] = -1;
+            ttt[1, 1] = -1;
+            ttt[1, 2] = -1;
+            ttt[2, 0] = -1;
+            ttt[2, 1] = -1;
+            ttt[2, 2] = -1;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+        }
+
+        private void quitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
